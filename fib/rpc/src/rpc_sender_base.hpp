edd38495addb2a2a8d06e90dbaf2da27dbbb9315
Aleksandar Dakic <aleksandar.dakic@zuehlke.com>
2022-06-05 11:12:09 +0200

    Define interface on RPC client side

---
+++ fib/rpc/src/rpc_sender_base.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "rpc_types.hpp"
+
+namespace fib::rpc
+{
+    class rpc_sender_base
+    {
+        rpc_sender_base(std::string address, int port);
+        virtual response fib(request req) = 0;
+        virtual ~rpc_sender_base()        = default;
+
+    protected:
+        std::string m_ip_address;
+        int m_port;
+    };
+}  // namespace fib::rpc
