241053adb771beb245fc02742759cad040f86c87
Aleksandar Dakic <aleksandar.dakic@zuehlke.com>
2022-06-06 10:03:52 +0200

    Turn off clang-tidy and clang-format by default

--- cmake/standard_settings.cmake
+++ cmake/standard_settings.cmake
@@ -1,2 +1,2 @@
-option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." ON)
-option(${PROJECT_NAME}_ENABLE_CLANG_FORMAT "Enable formating with Clang-Format." ON)
+option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." OFF)
+option(${PROJECT_NAME}_ENABLE_CLANG_FORMAT "Enable formating with Clang-Format." OFF)
